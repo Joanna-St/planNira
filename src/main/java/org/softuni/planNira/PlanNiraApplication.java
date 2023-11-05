d7bdc59de57134121281aa0205b080308f07e8fd
Joanna <daina6969@gmail.com>
2023-11-05 11:06:12 +0200

    Init commit

---
+++ src/main/java/org/softuni/planNira/PlanNiraApplication.java
@@ -0,0 +1,13 @@
+package org.softuni.planNira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanNiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanNiraApplication.class, args);
+	}
+
+}
